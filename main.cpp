0ee04ae27135876ed52b1ef1fe77231dc2f1b653
SAMUEL PANDOHAN TERAMPIL GULTOM <samuel.gultom@student.president.ac.id>
2022-08-18 14:12:22 +0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i, a, b) for(int i = a; i<=b; i++)
+#define PB push_back
+#define MP make_pair
+
+typedef long long int int64
+typedef unsigned long long int uint64
+
+int a, b, d, m, n
+
+void solve(){
+  cout << "Hello World"<< endl;
+}
+
+int main(){
+  ios::sync_with_stdio(0); cin.tie(0);
+
+  int i = 1; int tt;
+  cin >> tt;
+  while(tt--){
+    cout << "Case #"<<i<<" : ";
+    solve();
+    i++;
+  }
+
+  return 0;
+}
+
